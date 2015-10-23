a66aaf2cb48a3634af83e3f8e2eb17ccc89cbf4d
NimrodSV <nimrodsv@gmail.com>
2015-10-23 07:48:09 +0300

    Slim CMActions :)
    Change-Id: I837ec7c3554d8bb79385ba61e0c2130871e182b9

--- cmactions/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
+++ cmactions/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2015 The SlimRoms Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
 
 package com.cyanogenmod.settings.device;
 
-import com.android.internal.util.cm.ScreenType;
+import com.android.internal.util.slim.DeviceUtils;
 
 import android.app.ActionBar;
 import android.os.Bundle;
@@ -46,7 +46,7 @@ public class TouchscreenGestureSettings extends PreferenceActivity {
         super.onResume();
 
         // If running on a phone, remove padding around the listview
-        if (!ScreenType.isTablet(this)) {
+        if (!DeviceUtils.isTablet(this)) {
             getListView().setPadding(0, 0, 0, 0);
         }
     }
